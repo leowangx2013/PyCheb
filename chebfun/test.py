4e078702fcc353e4d4e559c43149a1f3001f7962
Leo <leowangx2013@outlook.com>
2016-10-03 15:29:04 +0800

    Fix several bugs

--- chebfun/test.py
+++ chebfun/test.py
@@ -1,9 +1,12 @@
 import PyCheb
 import numpy as np
 
-# transform from u to eye()
-'''
-	polyfuum.Polyfun(lfunc, rfunc, domain, lvalue, rvalue, N)
-'''
-f = PyCheb.ODEsolver("diff(u,2)", "math.exp(x)", [-1,1], 0, 0, precision=1e-11)
-f.solve(1)
+# Linear example
+# f = PyCheb.ODEsolver("diff(u,2)", "math.exp(x)", [-1,1], 0, 0, precision=1e-15)
+# Using N instead of precision
+#f = PyCheb.ODEsolver("diff(u,2)", "math.exp(x)", [-1,1], 0, 0, N=5)
+# f.solve(1)
+
+# Nonlinear example
+f = PyCheb.ODEsolver("diff(u,2)", "math.exp(2*u)", [-1,1], 0, 0,precision=1e-15)
+f.solve(0)
